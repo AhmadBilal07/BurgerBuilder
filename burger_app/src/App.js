97f52142e9648956b5406eedc32f76f35274f477
AhmadBilal07 <ahmadbilal07@live.com>
2020-10-26 10:22:54 +0100

    Layout Initial Setup Complete

--- burger_app/src/App.js
+++ burger_app/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello G </h1>
+        <Layout>
+          <h1>Hello G </h1>
+        </Layout>
       </div>
     );
   }
--- burger_app/src/components/Layout/Layout.js
+++ burger_app/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Aux from '../hoc/Auxiliary'
 
-const Layout = props => {
+const layout = props => (
     <Aux>
         <div>Side Drawer, Tool Bar, BackDrop</div>
         <main>
             {props.children}
         </main>
     </Aux>
-}
+);
+export default layout;
