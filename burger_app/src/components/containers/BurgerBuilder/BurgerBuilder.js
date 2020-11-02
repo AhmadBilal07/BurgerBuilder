9722919597a3ecd70717983c697fe4b76cc3e9d5
AhmadBilal07 <ahmadbilal07@live.com>
2020-11-02 03:05:05 +0100

    styling + control gui added

--- burger_app/src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_app/src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,22 @@
 import React,{Component} from 'react'
 import Aux from '../../hoc/Auxiliary'
+import Burger from '../../Burger/Burger'
+import BuildControls from '../../Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        }
+    }
     render(){
        return (
            <Aux>
-               <div>Burger</div>
-               <div>Build Control</div>
+               <Burger ingredients={this.state.ingredients}/>
+               <BuildControls/>
            </Aux>
        ); 
     }
