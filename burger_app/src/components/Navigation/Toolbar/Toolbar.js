42270e65671aaa4fdc0786c01b93db7419a67c11
AhmadBilal07 <ahmadbilal07@live.com>
2020-11-17 10:39:33 +0100

    navbar component added

--- burger_app/src/components/Navigation/Toolbar/Toolbar.js
+++ burger_app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import classes from "./Toolbar.css"
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+
 
 const toolbar = (props) => (
     <header className = {classes.ToolBar}>
         <div>MENU</div>
         <Logo/>
-        <nav>
-            ...
-        </nav>
+        <NavigationItems/>
     </header>
 );
 
